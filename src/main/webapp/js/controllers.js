3412934bdbd024cc861f302a22d7e79959451e8c
unclejamal <unclejamal@gmail.com>
2013-10-10 22:46:07 +0100

    scrolling works

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,11 +1,14 @@
 'use strict';
 
-/* Controllers */
-
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', function MyCtrl1($scope) {
+    var a = []
+    for (var i=0; i<50; i++) {
+        a.push({name: "App" + i});
+    }
+    $scope.apps = a;
 
-  }])
+  })
   .controller('MyCtrl2', [function() {
 
   }]);
