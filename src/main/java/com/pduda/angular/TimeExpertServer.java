701cbeeef7027e8d0a9f541ff997cf9abb628c36
unclejamal <unclejamal@gmail.com>
2013-10-10 20:50:58 +0100

    html5 mode working

--- src/main/java/com/pduda/angular/TimeExpertServer.java
+++ src/main/java/com/pduda/angular/TimeExpertServer.java
@@ -1,9 +1,10 @@
 package com.pduda.angular;
 
-import java.io.File;
+import org.eclipse.jetty.rewrite.handler.RewriteHandler;
+import org.eclipse.jetty.rewrite.handler.RewriteRegexRule;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
-import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -17,7 +18,9 @@ public class TimeExpertServer {
     public void start() {
         server = new Server(6666);
         HandlerCollection handlers = new HandlerCollection();
+        handlers.addHandler(rewriteHandler());
         handlers.addHandler(servletContextHandler());
+        handlers.addHandler(staticContentHandler());
         server.setHandler(handlers);
         try {
             server.start();
@@ -28,8 +31,14 @@ public class TimeExpertServer {
 
     private ServletContextHandler servletContextHandler() {
         ServletContextHandler handler = new ServletContextHandler();
-        handler.setContextPath("/");
-        handler.addServlet(new ServletHolder(new ServletContainer(resourceConfig())), "/rest/*");
+        handler.setContextPath("/rest");
+        handler.addServlet(new ServletHolder(new ServletContainer(resourceConfig())), "/*");
+        return handler;
+    }
+
+    private ServletContextHandler staticContentHandler() {
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.setContextPath("/static");
         handler.addServlet(new ServletHolder(new DefaultServlet()), "/*");
         handler.setResourceBase("src/main/webapp");
         handler.setClassLoader(Thread.currentThread().getContextClassLoader());
@@ -55,5 +64,20 @@ public class TimeExpertServer {
         } catch (InterruptedException e) {
             throw new RuntimeException("Could not join the thread", e);
         }
+    }
+
+    private Handler rewriteHandler() {
+        RewriteHandler rewrite = new RewriteHandler();
+        rewrite.setRewriteRequestURI(true);
+        rewrite.setRewritePathInfo(false);
+        rewrite.setOriginalPathAttribute("originalPath");
+
+        RewriteRegexRule reverse = new RewriteRegexRule();
+//        reverse.setRegex("/static/([^.])+$");
+        reverse.setRegex("/static/([^.])+$");
+        reverse.setReplacement("/index.html");
+        rewrite.addRule(reverse);
+
+        return rewrite;
     }
 }
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,9 +2,16 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {templateUrl: 'static/partials/partial1.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/view2', {templateUrl: 'static/partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
+
+myApp.config(['$locationProvider', function($locationProvider) {
+        $locationProvider
+                .html5Mode(true)
+                .hashPrefix('!');
+    }]);
